18d6936a67ada04f51618b7ef6b95477e2fc0a28
AYUSHI AGRAWAL <96446624+Ayushi01Agrawal@users.noreply.github.com>
2024-10-22 09:33:25 +0000

    Update variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,22 +1,32 @@
+//Declares a block-scoped, read-only named constant.
 const accountId = 144553  // const variable is not modified 
+
+// accountId=2  // not allowed 
+
+
+//Declares a block-scoped, local variable, optionally initializing it to a value.
 let accountEmail= "ayushi@gmail.com" // best practice to declared
+
+
+//Declares a variable, optionally initializing it to a value.
 var accountPassword = "12345" 
+/*
+Prefer Not to use var
+because of issue in block scope and functional scope
+ */
+
+
 accountCity="Jaipur"
 
 //variable without initialized is undefined 
 let accountState;
 
-// accountId=2  // not allowed 
-
+//examples:
 accountEmail="ay@hc.com"
 accountPassword="3456789"
 accountCity="Benguluru"
 
 console.log(accountId); // way to print variable.
 
-/*
-Prefer Not to use var
-because of issue in block scope and functional scope
- */
-
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) // way to print multiple variables values.
+// way to print multiple variables values.
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) 
