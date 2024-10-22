2b5f514276276bee76de182515383b133bf3addd
AYUSHI AGRAWAL <96446624+Ayushi01Agrawal@users.noreply.github.com>
2024-10-22 09:23:46 +0000

    learn about data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+" use strict"; // treat all JS code as newer version
+
+// alert("Hello")   /* throw an error because we are using Node js , not browser */
+
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("Ayuhsi")
+
+
+let name="ayushi"
+let age=18
+let isLoggedIn= false
+
+
+/*
+number -> 2^53
+bigint
+string ->""
+boolean -> true/false
+null => standalone value "It is an object"
+undefined=>
+symbol => unique
+
+object
+*/
+
+console.log(typeof undefined) // type => undefined
+console.log(typeof null) // type=> object
+
